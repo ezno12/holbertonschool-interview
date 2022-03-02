900f6ef9d929f60e402a8732d36a6fcf4b05636b
tennin12 <bassembenyhia12@gmail.com>
2022-03-02 23:57:09 +0100

    f0 added

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+module containing function to calc min operations
+"""
+
+import re
+
+
+def minOperations(n):
+    """
+    calc min operations
+    """
+
+    if n <= 1:
+        return 0
+    else:
+        prime = prime_numbers(n)
+        if prime == []:
+            return 0
+        else:
+            return sum(prime)
+
+
+def prime_numbers(n):
+    """
+    calc
+    """
+    if n < 2:
+        return []
+    
+    prime = []
+    i = 2
+    while i <= n:
+        if n % i == 0:
+            prime.append(i)
+            n /= i
+        else:
+            i += 1
+    return prime
