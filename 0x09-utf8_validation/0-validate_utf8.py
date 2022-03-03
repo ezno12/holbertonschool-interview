14cf6e0ffdcc91f12d81ce20319e930485701d8a
tennin12 <bassembenyhia12@gmail.com>
2022-03-03 02:36:06 +0100

    f0 added

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,13 @@
+def validUTF8(data):
+    """
+    """
+    successive_10 = 0
+    for b in data:
+        b = bin(b).replace('0b','').rjust(8, '0')
+        if successive_10 != 0:
+            successive_10 -= 1
+            if not b.startswith('10'):
+                return False
+        elif b[0] == '1':
+                successive_10 = len(b.split('0')[0]) - 1
+    return True
